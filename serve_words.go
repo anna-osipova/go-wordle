8208bbabd13c07b671c88c19394d5bc2df052b48
Anna Osipova <anna.osipova@smartly.io>
2022-01-17 09:51:24 +0000

    Add hint endpoint

--- serve_words.go
+++ serve_words.go
@@ -26,6 +26,12 @@ type Letter struct {
 	Color         string `json:"color`
 }
 
+type WordHelp struct {
+	Include string `json:"include" binding:"required"`
+	Exclude string `json:"exclude" binding:"required"`
+	Template string `json:"template" binding:"required"`
+}
+
 func CountExistingLetters(letters []Letter, letter string) int {
 	count := 0
 	for _, n := range letters {
@@ -45,6 +51,31 @@ func CountExactMatches(word string, guess_word string, letter string) int {
 		}
 	}
 	return count
+}
+
+func IsMatch(word_help *WordHelp, word string) bool {
+	// word contains letters from "include"
+	for _, n := range word_help.Include {
+		if strings.Index(word, string(n)) < 0 {
+			return false
+		}
+	}
+	// word doesn't contain letters from "exclude"
+	for _, n := range word_help.Exclude {
+		if strings.Index(word, string(n)) > -1 {
+			return false
+		}
+	}
+	// word matches "template"
+	if word_help.Template != "" {
+		for i, n := range word_help.Template {
+			letter := string(n)
+			if letter != "_" && string(word[i]) != letter {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 func main() {
@@ -110,6 +141,21 @@ func main() {
 			})
 		}
 		c.JSON(200, letters)
+	})
+
+	r.POST("/words/hint", func(c *gin.Context) {
+		var word_help WordHelp
+		c.BindJSON(&word_help)
+
+		var matching_words []string
+		for _, word := range words {
+
+			if IsMatch(&word_help, word) == true {
+				matching_words = append(matching_words, word)
+			}
+		}
+
+		c.JSON(200, gin.H{ "matches": matching_words })
 	})
 	r.Run()
 }
