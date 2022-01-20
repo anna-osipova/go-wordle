7a45afeef49aa06785152afc0b2f1c664d9fad07
Anna Osipova <anna.osipova@smartly.io>
2022-01-20 15:20:05 +0000

    Add jwt service

---
+++ service/auth_jwt.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+//jwt service
+type JWTService interface {
+	GenerateToken(word string) string
+	ValidateToken(token string) (*jwt.Token, error)
+}
+type authCustomClaims struct {
+	Word string `json:"word"`
+	jwt.StandardClaims
+}
+
+type jwtServices struct {
+	secretKey string
+	issure    string
+}
+
+func JWTAuthService() JWTService {
+	return &jwtServices{
+		secretKey: getSecretKey(),
+		issure:    "Anna",
+	}
+}
+
+func getSecretKey() string {
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		secret = "secret"
+	}
+	return secret
+}
+
+func (service *jwtServices) GenerateToken(word string) string {
+	claims := &authCustomClaims{
+		word,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 48).Unix(),
+			Issuer:    service.issure,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	//encoded string
+	t, err := token.SignedString([]byte(service.secretKey))
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func (service *jwtServices) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	return jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		if _, isvalid := token.Method.(*jwt.SigningMethodHMAC); !isvalid {
+			return nil, fmt.Errorf("Invalid token", token.Header["alg"])
+		}
+		return []byte(service.secretKey), nil
+	})
+}
