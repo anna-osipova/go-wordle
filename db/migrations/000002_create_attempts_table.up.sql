a48382d6b1376d1228c693a50e3f9544f3f35d61
Anna Osipova <anna.osipova@smartly.io>
2022-01-24 22:21:15 +0000

    Add DB

---
+++ db/migrations/000002_create_attempts_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS attempts (
+  id SERIAL PRIMARY KEY,
+  session_id INTEGER,
+  word VARCHAR(5),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  constraint fk_sessions_attempts
+     foreign key (session_id) 
+     REFERENCES sessions (id)
+);
