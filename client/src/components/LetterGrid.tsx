f1c52be25373c1c37a5304b6c3240e9e77abf76d
Anna Osipova <anna.osipova@smartly.io>
2022-02-08 12:29:20 +0200

    Add tile animations

--- client/src/components/LetterGrid.tsx
+++ client/src/components/LetterGrid.tsx
@@ -15,10 +15,20 @@ export const LetterGrid = ({ attempts, input }: LetterGridProps) => {
       {Array(MAX_ATTEMPTS)
         .fill(0)
         .map((_, row) => {
+          if (row === attempts.length - 1) {
+            return attempts[row].letters.map((letter, i) => (
+              <div className={`cell-container column-${i}`} key={`${row}-${i}`}>
+                <div className="cell-card">
+                  <div className={`cell cell-card-back color-${letter.color}`}>{letter.letter}</div>
+                  <div className="cell cell-card-front">{letter.letter}</div>
+                </div>
+              </div>
+            ));
+          }
           if (attempts[row]) {
             return attempts[row].letters.map((letter, i) => (
-              <div className={`cell cell-${row}-${i} color-${letter.color}`} key={`${row}-${i}`}>
-                {letter.letter}
+              <div className="cell-container" key={`${row}-${i}`}>
+                <div className={`cell color-${letter.color}`}>{letter.letter}</div>
               </div>
             ));
           }
@@ -26,14 +36,18 @@ export const LetterGrid = ({ attempts, input }: LetterGridProps) => {
             return Array(WORD_LENGTH)
               .fill(0)
               .map((_, i) => (
-                <div className={`cell cell-${row}-${i}`} key={`${row}-${i}`}>
-                  {input[i]}
+                <div className="cell-container" key={`${row}-${i}`}>
+                  <div className={`${input[i] ? 'cell' : 'cell-empty'}`}>{input[i]}</div>
                 </div>
               ));
           }
           return Array(WORD_LENGTH)
             .fill(0)
-            .map((_, i) => <div className={`cell cell-${row}-${i}`} key={`${row}-${i}`}></div>);
+            .map((_, i) => (
+              <div className="cell-container" key={`${row}-${i}`}>
+                <div className="cell-empty"></div>
+              </div>
+            ));
         })}
     </div>
   );
