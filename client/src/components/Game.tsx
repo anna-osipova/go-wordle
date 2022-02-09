884f9e3f817b247d9fa4a5c1c34943a5f3836a85
Anna Osipova <anna.osipova@smartly.io>
2022-02-09 16:09:42 +0200

    Improve UI

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -123,6 +123,10 @@ export const Game = () => {
         layout={{
           default: ['q w e r t y u i o p', 'a s d f g h j k l', '{enter} z x c v b n m {bksp}']
         }}
+        display={{
+          '{enter}': 'ENTER',
+          '{bksp}': '⬅'
+        }}
       />
       {gameState === GameState.Lost && (
         <div className="correct-word">
